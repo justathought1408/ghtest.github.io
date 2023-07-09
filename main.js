e4f78f984e7ec732a9bba713a9b5e929179b3bb0
JTh <justathought1408@gmail.com>
2023-07-09 21:05:04 +0300

    test

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ let btn = document.querySelector("button");
 
 btn.addEventListenter("click", () => {
 	let data = {
-		one: document.querySelector("input.one").value;
-		two: document.querySelector("input.two").value;
-		three: document.querySelector("input.three").value;
+		one: document.querySelector("input.one").value,
+		two: document.querySelector("input.two").value,
+		three: document.querySelector("input.three").value,
 	};
 	tg.close();
 	tg.sendData(JSON.stringify(data));
